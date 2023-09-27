ebc280064b23bf1c2aa72fe5d8f31df1f0787a24
Erik Morén <erik.moren78@gmail.com>
2023-09-27 18:06:34 +0200

    Har lagt till Newtonsoft.Json och lite test kod

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -1,10 +1,49 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
 namespace AEAQuiz
 {
+    public class Settings { }
     public partial class SettingsPage : ContentPage
     {
         public SettingsPage()
         {
             InitializeComponent();
+
+            string schemaJson = @"{
+  'description': 'A person',
+  'type': 'object',
+  'properties':
+  {
+    'name': {'type':'string'},
+    'hobbies': {
+      'type': 'array',
+      'items': {'type':'string'}
+    }
+  }
+}";
+
+            JsonSchema schema = JsonSchema.Parse(schemaJson);
+
+            JObject person = JObject.Parse(@"{
+  'name': 'James',
+  'hobbies': ['.NET', 'Blogging', 'Reading', 'Xbox', 'LOLCATS']
+}");
+
+            bool valid = person.IsValid(schema);
+            if (!valid)
+            {
+                JsonResult.Text = "JSON not so good";
+            }
+            else
+            {
+                JsonResult.Text = $"{person["name"]} has Hobbies: \n\t";
+                foreach (var item in person["hobbies"]) 
+                {
+                    JsonResult.Text += $"{item}, ";
+                }
+            }
         }
 
         void OnSliderValueChanged(object sender, ValueChangedEventArgs args)

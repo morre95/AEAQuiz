71ceea1cb75ad9c2dbc2851cda43975b5f6d787b
hammarhank <axtr@live.com>
2023-09-27 11:32:29 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AEAQuiz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

ba635386f13a7efe86b8dec4a2c4a9e8a6b16b9b
morre95 <erik.moren78@gmail.com>
2023-09-28 13:30:06 +0200

    Skapade sidor

---
+++ Pages/GamePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AEAQuiz.Pages;
+
+public partial class GamePage : ContentPage
+{
+	public GamePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Pages/GameSettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AEAQuiz.Pages;
+
+public partial class GameSettingsPage : ContentPage
+{
+	public GameSettingsPage()
+	{
+		InitializeComponent();
+	}
+}

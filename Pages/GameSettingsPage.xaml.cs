77620ed033ef02a9128bedd31b3202cee8995f8a
morre95 <erik.moren78@gmail.com>
2023-09-28 15:21:37 +0200

    Har designat game sidorna

--- Pages/GameSettingsPage.xaml.cs
+++ Pages/GameSettingsPage.xaml.cs
@@ -5,5 +5,25 @@ public partial class GameSettingsPage : ContentPage
 	public GameSettingsPage()
 	{
 		InitializeComponent();
-	}
+        DificultyPicker.SelectedIndex = 0;
+        TypePicker.SelectedIndex = 0;
+        NumQuestionsSlider.Value = 15;
+
+    }
+
+    private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        //Avrunda värdet från slidern
+        var roundedValue = Math.Round(e.NewValue);
+
+        NumQuestionsSlider.Value = roundedValue;
+
+        displayLabel.Text = roundedValue.ToString();
+        //TODO: 
+    }
+
+    private async void GameBtn_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new GamePage());
+    }
 }

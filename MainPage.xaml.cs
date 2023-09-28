a999aff28dabdd164aaa5b2416483057da935621
Amir Hebib <98492804+hammarhank@users.noreply.github.com>
2023-09-28 10:15:19 +0200

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,21 +9,7 @@
             InitializeComponent();
         }
 
-<<<<<<< HEAD
-=======
-        //private void OnCounterClicked(object sender, EventArgs e)
-        //{
-        //    count++;
 
-        //    if (count == 1)
-        //        CounterBtn.Text = $"Clicked {count} time";
-        //    else
-        //        CounterBtn.Text = $"Clicked {count} times";
-
-        //    SemanticScreenReader.Announce(CounterBtn.Text);
-        //}
-
->>>>>>> c72e196 (Gui förstasida. ny bakgrund, rensat från andra knappar och tillagt settings i top höger.)
         private async void SettingsBtn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new SettingsPage());

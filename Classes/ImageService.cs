20b9feed8814552976af049cccd190df6b31e3d8
hammarhank <axtr@live.com>
2023-10-02 14:02:42 +0200

    imageService tillagd samt tre bilder per kategori.

---
+++ Classes/ImageService.cs
@@ -0,0 +1,18 @@
+﻿namespace AEAQuiz.Classes
+{
+    public class ImageService
+    {
+        private Random _random = new Random();
+
+        public ImageSource GetRandomImageForCategory(int category)
+        {
+
+            int imageNumber = _random.Next(1, 4);
+
+            string imagePath = $"Images/CategoryImg/{category}/{category}-{imageNumber}.png";
+
+            return ImageSource.FromFile(imagePath);
+        }
+    }
+
+}

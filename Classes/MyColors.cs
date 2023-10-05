ee9648e663df496d307b39f595285bef29c89977
Erik Morén <erik.moren78@gmail.com>
2023-10-05 10:40:31 +0200

    Ändrat färger

--- Classes/MyColors.cs
+++ Classes/MyColors.cs
@@ -2,25 +2,26 @@
 {
     internal class MyColors
     {
-        private static readonly List<Color> _colors2 = new List<Color> 
+        private static readonly List<Color> _colors = new List<Color> 
         {
-            Colors.Green,
-            Color.FromUint(0x808080),
-            Colors.MediumBlue,
-            Color.FromUint(0x997300),
-            Color.FromUint(0x669900),
-            Color.FromUint(0x99005c),
-            Color.FromUint(0xff0000),
-            Color.FromUint(0x000000),
+            Colors.Black,
             Colors.Blue,
-            Color.FromUint(0x4cb8ff)
+            Colors.Green,
+            Colors.Silver,
+            Colors.Olive,
+            Colors.Navy,
+            Colors.Maroon,
+            Colors.Gray,
+            Colors.Teal,
+            Colors.Purple
+            //Color.FromUint(0x4cb8ff)
         };
 
         public static Color GetColorBy(int index)
         {
             string str = index.ToString();
             char lastCharacter = str[str.Length - 1];
-            return _colors2[int.Parse(lastCharacter.ToString())];
+            return _colors[int.Parse(lastCharacter.ToString())];
         }
 
     }
